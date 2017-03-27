4747deb6b0f482e270c83ade156f969f343f34d2
jerry <jerryshew@gmail.com>
2017-03-27 19:41:14 +0800

    Update formatNumber.js

--- formatNumber.js
+++ formatNumber.js
@@ -1,1 +1,7 @@
-
+function formatNumber(str) {  
+    if(str.length <= 3){  
+        return str;  
+    } else {  
+        return formatNumber(str.substr(0,str.length-3))+','+str.substr(str.length-3);  
+    }  
+}
